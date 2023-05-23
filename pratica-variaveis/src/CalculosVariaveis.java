90b66a1acc3e5ce2b68df4d1d9ef5c51292b3ee0
S4mukk <samuelcomgeorge@gmail.com>
2023-05-23 13:00:34 -0300

    Prática de calculos com variáveis

---
+++ pratica-variaveis/src/CalculosVariaveis.java
@@ -0,0 +1,24 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class CalculosVariaveis {
+    public static void main(String[] args) {
+
+       int n1 = 3;
+       int n2 = 2;
+       int soma = n1 + n2;
+       int multiplicacao = n1 * n2;
+       float divisao = (float) n1 / n2;
+       int subtracao = n1 - n2;
+
+       System.out.println("Soma: " + soma);
+       System.out.println("Multiplicação: " + multiplicacao);
+       System.out.println("Divisão: " + divisao);
+       System.out.println("Subtração: " + subtracao);
+
+
+
+
+
+
+    }
+}
